e20800d375c3c7170132d737c19b7bfc6368a151
Eric <gnijuohz@gmail.com>
2016-07-26 18:39:04 -0700

    add ipython notebook

---
+++ leetcode.py
@@ -0,0 +1,6 @@
+# login page: https://leetcode.com/accounts/login/
+
+<input class="form-control" autofocus="autofocus" id="id_login" name="login" placeholder="Username or e-mail" type="text">
+<input class="form-control" id="id_password" name="password" placeholder="Password" type="password">
+<input id="id_remember" name="remember" type="checkbox">
+<button class="btn btn-primary" type="submit">Sign In</button>
